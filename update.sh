0406755c9581584efae85664364d2c53b10c9fbb
root <root@coadmin.org>
2021-10-18 21:26:56 +0000

    pm2check

--- update.sh
+++ update.sh
@@ -3,12 +3,25 @@
 # CWD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 CWD="/opt/coadmin-peer"
 
+
+
+pm2check() {
+    # usage <name> <folder> <filename> <parameters>
+    # example: pm2check resolver /opt/ns3-resolver/ resolver.js ""
+    pm2 describe "$1" >/dev/null || { cd "$2" && pm2 start --log-date-format "YYYY-MM-DD HH:mm:ss" "$3" --name "$1" -- "$4" && pm2 save; sl "$1 has been started"; }
+    pm2 describe "$1" | egrep -q "status.*(errored|stopped)" && { pm2 restart "$1"; sl "$1 has been re-started"; }
+}
+
+
+
 hostname | egrep '^coadmin.org$' && {
     echo "we should not run on main server, since this is dev"
     exit 0
 }
 
 test -e /opt/coadmin-peer-dev && exit
+
+
 
 ## install ourself into crontab
 test -h "/etc/cron.hourly/coadmin-update" || ln -s "$CWD/update.sh" /etc/cron.hourly/coadmin-update
@@ -30,5 +43,8 @@ EOF
 git stash
 git pull
 npm install 
+
+
+pm2check coadmin /opt/coadmin-peer peer.js ""
 
 echo "Done"
