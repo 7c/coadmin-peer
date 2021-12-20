d1ba1eaaa76730695274dc01e5fe27ebfc4e9ff0
root <root@coadmin.org>
2021-12-20 13:00:29 +0000

    cron tests

---
+++ __cron_minutely.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+mainpid=$$
+(sleep 1m; kill $mainpid) &
+watchdogpid=$!
+bye() {
+    echo "."
+    kill -9 $watchdogpid
+}
+
+report_test_ok() { 
+    local id="$1"
+    local details="$2"
+    test -z $2 && details="-"
+    test -e /opt/coadmin-peer/tools/report_test.js && {
+        node /opt/coadmin-peer/tools/report_test.js --project 'system' --group 'system' --id "$id" --result "ok" --details "$details"
+    }
+}
+
+isInstalled() {
+    dpkg -s $1 2>&1 | grep -q 'is not installed and no information'  && return 1
+    dpkg -s $1 2>&1 | grep Status | grep -q 'not-installed'          && return 2
+    dpkg -s $1 2>&1 | grep Status | grep -q 'half-installed'         && return 3
+    dpkg -s $1 2>&1 | grep Status | grep -q 'half-configured'         && return 4
+    return 0
+}
+
+report_test_error() {
+    local id="$1"
+    local details="$2"
+    test -z $2 && details="-"
+    test -e /opt/coadmin-peer/tools/report_test.js && {
+        node /opt/coadmin-peer/tools/report_test.js --project 'system' --group 'system' --id "$id" --result "error" --details "$details"
+    }
+}
+
+ensure_processRunning() {
+    if pgrep "$1" >/dev/null; then report_test_ok "$2"; else report_test_error "$2"; fi
+}
+
+### tests
+
+
+## nginx is installed but not running
+isInstalled nginx && ensure_processRunning nginx "nginx is running"
+ensure_processRunning cron "cron is running"
+ensure_processRunning vnstatd "vnstatd is running"
+
+
+bye
