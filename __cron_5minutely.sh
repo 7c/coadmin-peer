8ae3e35dc5271e747625bbf693bfb2f3bcbf08ef
root <root@coadmin.org>
2022-01-22 13:54:02 +0000

    nginx too many open files detectin

--- __cron_5minutely.sh
+++ __cron_5minutely.sh
@@ -122,7 +122,8 @@ fi
 
 ## nginx 'Too many open files' error detection
 test -d /var/log/nginx && {
-    if tail /var/log/nginx/*log -n 100 | grep -i -q 'Too many open files'; then
+    ago5=$(date --date='5 minutes ago' +"%Y/%m/%d %H:%M")
+    if tail /var/log/nginx/*log -n 1000 | grep "$ago5" | grep -i -q 'Too many open files'; then
         report_test_error "nginx too many open files"
     else
         report_test_ok "nginx too many open files"
