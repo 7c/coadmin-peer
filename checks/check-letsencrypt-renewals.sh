a0e72c8cbbb2fbed5bc228fef1a7d8917dae6298
root <root@coadmin.org>
2022-06-18 12:29:45 +0000

    certbot installation check if renewal folder is there

--- checks/check-letsencrypt-renewals.sh
+++ checks/check-letsencrypt-renewals.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 test -d /etc/letsencrypt/renewal && {
+    if isInstalled certbot; then
+        report_test_ok "certbot installed"
+    else
+        report_test_error "certbot installed"
+    fi
+
     if npx lefolder; then
         report_test_ok "letsencrypt renewals"
     else
